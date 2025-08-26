cc893d7656fa42e7e5fc434133643b275e8812bc
Ganesh Sabat <77581432+ganesh195@users.noreply.github.com>
2025-08-26 10:08:29 +0530

    First commit

---
+++ dijkstra.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+
+class Dijkstra {
+    
+    static class Node implements Comparable<Node> {
+        int vertex;
+        int distance;
+        
+        Node(int vertex, int distance) {
+            this.vertex = vertex;
+            this.distance = distance;
+        }
+        public int compareTo(Node other) {
+            return 
+        }
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+    }
+}
